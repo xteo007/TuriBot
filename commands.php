ad608e141f6f755165eed3c404d93b5e3637638f
davtur19 <dav.tur19@gmail.com>
2017-11-14 17:14:51 +0100

    Fix text

--- commands.php
+++ commands.php
@@ -2,32 +2,32 @@
 require "bot.php";
 
 
-//test if bot work and print on page the name of the bot
+//tests if the bot works and prints on page the name of the bot
 getMe(true);
 
 
-//save last request for debug
+//saves last request for debug
 $file = "update.json";
 $f = fopen($file, 'w');
 fwrite($f, $content);
 fclose($f);
 
 
-//a simple message when /start or /help received from update
+//a simple message when /start or /help is received from update
 if(stripos($message_text, "/start")===0 or stripos($message_text, "/help")===0)
 {
 	sendMessage($message_chat_id, "/help - Show this message\n/license - Sends you the link of the page with the license and source code of the bot");
 }
 
 
-//show license of the bot
+//shows license of the bot
 if(stripos($message_text, "/license")===0)
 {
 	sendMessage($message_chat_id, "Copyright (C) 2017 Davide Turaccio\nThis program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or at your option) any later version.\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.\nYou should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.\n\nThe source code of this bot is avaible on https://github.com/davtur19/php-telegram-bot-api");
 }
 
 
-//a message with the user's name, response to the user's message, and a buttons
+//a message with the user's name, response to the user's message, and buttons
 if(stripos($message_text, "/message")===0)
 {
 	$menu['inline_keyboard'] = array(
@@ -52,6 +52,7 @@ if(stripos($message_text, "/message")===0)
 }
 
 
+//edits the message and notifies the user
 if(stripos($callback_query_data, "btn1")===0)
 {
 	$menu['inline_keyboard'] = array(
