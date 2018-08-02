de4b5df38edecf4116a8c98e41b15a789ac8a050
davtur19 <dav.tur19@gmail.com>
2018-08-02 19:52:44 +0200

    add config.php

--- bot.php
+++ bot.php
@@ -18,17 +18,6 @@
  *
  **/
 
-//replace 1111 with your id for debug() function
-define('MYID', 1111);
-//set the nickname of the bot for the recognition of the commands, lowercase!
-define('NICKNAMEBOT', '@bot');
-//put false if you want to use json payload for faster speed. with some server configuration it may not work properly, moreover the first request will not receive any reply from telegram
-define('RESPONSE', true);
-//if you do not want to use the variables generated automatically by the telegram update but directly the array of $update put the parameter below to false
-define('EASY_VAR', true);
-
-
-
 if (!isset($_GET['api'])) {
     exit();
 }
