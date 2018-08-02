a79a9dc2e1fcd3f3b55f98ce8d8525c11db197da
davtur19 <dav.tur19@gmail.com>
2018-08-02 20:18:42 +0200

    better debug()

--- functions/debug.php
+++ functions/debug.php
@@ -4,9 +4,6 @@ function debug($debug)
 {
     global $chat_id;
 
-    if (!isset($chat_id) or $chat_id == 0 or $chat_id == '') {
-        $chat_id = MYID;
-    }
 
     $str = print_r($debug, true);
     $array_str = str_split($str, 4050);
